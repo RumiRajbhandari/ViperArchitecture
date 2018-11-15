48af9a10d6ac70e96565657f9497682d664501ac
rumi <rumi@rumis-macbook-pro.local>
2018-11-15 13:10:52 +0545

    Initial Commit

---
+++ ViperArchitectureDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViperArchitectureDemo
+//
+//  Created by rumi on 11/15/18.
+//  Copyright © 2018 rumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
